547d743ade65032481b361d220a9c340606abe82
Mookyung Kwak <pporri@yonsei.ac.kr>
2019-03-30 11:30:50 +0900

    modification for handling error & changes in logic

--- gamerankings/spiders/games.py
+++ gamerankings/spiders/games.py
@@ -8,43 +8,59 @@ class GameReviewSpider(scrapy.Spider):
 
     # hand out every pages to "parse games list" method
     def start_requests(self):
-        for i in range(0, 2): # 259 is the last page
-            url = 'https://www.gamerankings.com/browse.html?page={}&sort=5&numrev=1'.format(i)
+        # step 1: crawl every games through 2011 
+        for i in range(0, 500): # 499 is the last page limit
+            url = 'https://www.gamerankings.com/browse.html?page={}&sort=5&numrev=2'.format(i)
             request = scrapy.Request(url=url, callback=self.parseGamesList)
             yield request
 
+        # step 2: from 2011 to 2019 every 70 pages
+        for i in range (2011, 2020):
+            for j in range (0, 70):
+                url = 'https://www.gamerankings.com/browse.html?year={}page={}&sort=5&numrev=2'.format(i, j)
+                request = scrapy.Request(url=url, callback=self.parseGamesList)
+                yield request
+
+
     # parse lists in the page into seperate games
     def parseGamesList(self, response):
-        for link in response.xpath('//div[@class="body"]/table/tr/td[3]/a/@href').extract():
-            link = '/'.join(link.split('/')[:-1])
-            url = 'http://www.gamerankings.com' + link + '/articles.html'
-            request = scrapy.Request(
-                url=url, 
-                callback=self.parseGame, 
-                meta={'dont_redirect': False, 
-                    'handle_httpstatus_list': [302]})
-            yield request
+        try:
+            for link in response.xpath('//div[@class="body"]/table/tr/td[3]/a/@href').extract():
+                link = '/'.join(link.split('/')[:-1])
+                url = 'http://www.gamerankings.com' + link + '/articles.html'
+                request = scrapy.Request(
+                    url=url, 
+                    callback=self.parseGame)
+                yield request
+        except IndexError:
+            pass
 
     # extract article from review page and store into the object
     def parseGame(self, response):
-        reviews = response.xpath('//table[@class="release"]/tbody/tr')
-        for review in reviews:
-            # prepare the object to store
-            item = ReviewItem()
-            
-            # article extractor borrowed from https://github.com/misja/python-boilerpipe
-            link = review.xpath('.//td[3]/a/@href').extract()[0]
-            extractor = Extractor(extractor='ArticleExtractor', url=link)
+        try:
+            reviews = response.xpath('//table[@class="release"]/tbody/tr')
+            for review in reviews:
+                # prepare the object to store
+                item = ReviewItem()
+                
+                # article extractor borrowed from https://github.com/misja/python-boilerpipe
+                link = review.xpath('.//td[3]/a/@href').extract()[0]
+                try:
+                    extractor = Extractor(extractor='ArticleExtractor', url=link)
+                    item['content'] = extractor.getText()
+                except Exception as e:
+                    item['content'] = str(e)
 
-            # detailed informations
-            item['source'] = review.xpath('.//td[1]/a/text()').extract()[0]
-            item['date'] = review.xpath('.//td[2]/text()').extract()[0]
-            item['game_name'] = response.xpath('//h1/text()').extract()[0]
-            item['game_tag'] = response.xpath('//div[@class="crumbs"]/a/text()').extract()
-            item['score'] = review.xpath('.//td[4]/text()').extract()[0]
-            item['url'] = link
-            item['content'] = extractor.getText()
-            print(item)
+                # detailed informations
+                item['source'] = review.xpath('.//td[1]/a/text()').extract()[0]
+                item['date'] = review.xpath('.//td[2]/text()').extract()[0]
+                item['game_name'] = response.xpath('//h1/text()').extract()[0]
+                item['game_tag'] = response.xpath('//div[@class="crumbs"]/a/text()').extract()
+                item['score'] = review.xpath('.//td[4]/text()').extract()[0]
+                item['url'] = link
+                print(item)
 
-            # send to pipeline
-            yield item
+                # send to pipeline
+                yield item
+        except IndexError:
+            pass
